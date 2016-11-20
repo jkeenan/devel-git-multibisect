95ad40cf52ad420129c8cf74da5cf9607beaef03
James E Keenan <jkeenan@cpan.org>
2016-11-20 09:13:06 -0500

    CPAN version 0.08.
    Exclude from Windows, as too many different problems are being reported by
    CPANtesters on Windows to be able to resolve without access to a Windows box
    with git installed.
    
    To improve chances of favorable results from ultra-strict CPANtesters
    regimens, add ExtUtils::MakeMaker to PREREQ_PM.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.06', new => '0.07' },
-    date    => { old => 'November 12 2016', new => 'November 16 2016' },
+    version => { old => '0.07', new => '0.08' },
+    date    => { old => 'November 16 2016', new => 'November 20 2016' },
     copyright => { old => '2016', new => '2016' },
 );
 
--- lib/Devel/Git/MultiBisect/AllCommits.pm
+++ lib/Devel/Git/MultiBisect/AllCommits.pm
@@ -14,7 +14,7 @@ use Cwd;
 use File::Temp;
 use List::Util qw(first sum);
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 NAME
 
