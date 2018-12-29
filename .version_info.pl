a06c225e355af6442310a980ff3744918791c18a
James E Keenan <jkeenan@cpan.org>
2018-12-29 12:22:36 -0500

    CPAN version 0.10
    Combine 4 test files into 1 to avoid problems when someone is running
    'make test' in parallel (HARNESS_OPTIONS=j2 or greater).
    
    Thanks to Slaven and Andreas for diagnostics.
    
    Update $VERSION and relevant dates.  Update Changes.

--- .version_info.pl
+++ .version_info.pl
@@ -1,5 +1,5 @@
 %trans = (
-    version => { old => '0.08', new => '0.09' },
+    version => { old => '0.09', new => '0.10' },
     #    date    => { old => 'November 16 2016', new => 'November 20 2016' },
     #    copyright => { old => '2016', new => '2016' },
 );
