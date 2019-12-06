b5000fb349e877928d07eea2baa5108efb8bf0fa
James E Keenan <jkeenan@cpan.org>
2019-12-06 13:12:17 -0500

    CPAN version 0.13
    Update $VERSION and release date.

--- .version_info.pl
+++ .version_info.pl
@@ -1,5 +1,5 @@
 %trans = (
-    version => { old => '0.11', new => '0.12' },
+    version => { old => '0.12', new => '0.13' },
     #    date    => { old => 'November 16 2016', new => 'November 20 2016' },
     #    copyright => { old => '2016', new => '2016' },
 );
