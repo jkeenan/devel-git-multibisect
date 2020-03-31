e15eeac84781ff3fb346c2bbbd951fd218ceed38
James E Keenan <jkeenan@cpan.org>
2020-03-31 08:49:45 -0400

    CPAN version 0.14
    Add xt/warnings-transitions.pl.

--- .version_info.pl
+++ .version_info.pl
@@ -1,5 +1,5 @@
 %trans = (
-    version => { old => '0.12', new => '0.13' },
+    version => { old => '0.13', new => '0.14' },
     #    date    => { old => 'November 16 2016', new => 'November 20 2016' },
     #    copyright => { old => '2016', new => '2016' },
 );
