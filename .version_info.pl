8619912672e93ce1696bc708a8da8193089f50a4
James E Keenan <jkeenan@cpan.org>
2021-09-01 08:22:16 -0400

    CPAN version 0.19
    Specify Data::Dump in Makefile.PL.  Update Changes and $VERSION.

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.17', new => '0.18' },
-    date    => { old => 'August 30 2021', new => 'August 30 2021' },
+    version => { old => '0.18', new => '0.19' },
+    date    => { old => 'August 30 2021', new => 'September 1 2021' },
     #    copyright => { old => '2016', new => '2016' },
 );
 
--- lib/Devel/Git/MultiBisect/Auxiliary.pm
+++ lib/Devel/Git/MultiBisect/Auxiliary.pm
@@ -1,7 +1,7 @@
 package Devel::Git::MultiBisect::Auxiliary;
 use v5.14.0;
 use warnings;
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 $VERSION = eval $VERSION;
 use base qw( Exporter );
 our @EXPORT_OK = qw(
